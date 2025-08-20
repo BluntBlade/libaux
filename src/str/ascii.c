bbf2d39fff9600ac527afd72eee3dd9561938953
Liang Tao <amethyst.black@gmail.com>
2025-08-20 15:39:49 +0800

    FIX: 修正 ascii_count() 的代码逻辑，chars <= 0 时以 size 作为字符数上限。

--- src/str/ascii.c
+++ src/str/ascii.c
@@ -6,7 +6,7 @@ int32_t ascii_count(const char_t * start, int32_t size, int32_t * chars)
     int32_t i = 0;
     int32_t max = 0;
 
-    max = (chars && *chars < size) ? *chars : size;
+    max = (chars && 0 < *chars && *chars < size) ? *chars : size;
     for (pos = start; i < max; ++i, ++pos) {
         if (ascii_measure(pos) == 0) return -1;
     } // for
