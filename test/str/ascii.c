c76cdfb72d5af630731773801ad48a6bb549b213
Liang Tao <amethyst.black@gmail.com>
2025-08-20 15:38:46 +0800

    ADD: 添加测试用例。

--- test/str/ascii.c
+++ test/str/ascii.c
@@ -22,3 +22,49 @@ Test(Function, ascii_measure)
     ch = '\x80';
     cr_expect(ascii_measure(&ch) == 0, "ascii_measure('\\x80') return 1");
 } // ascii_measure
+
+Test(Function, ascii_count)
+{
+    const char_t s1[] = {"A"};
+    const char_t s2[] = {"AB"};
+    const char_t s3[] = {"\x20\x40\x7F"};
+
+    const char_t b1[] = {"\0"};
+    const char_t b2[] = {"A\0"};
+    const char_t b3[] = {"\0AB"};
+
+    int32_t size = 0;
+    int32_t bytes = 0;
+    int32_t chars = 0;
+
+    size = sizeof(s1) - 1;
+    bytes = ascii_count(s1, size, &chars);
+    cr_expect(bytes == size, "ascii_count(s1) return incorrect bytes: expect %d, got %d", size, bytes);
+    cr_expect(chars == size, "ascii_count(s1) return incorrect chars: expect %d, got %d", size, chars);
+
+    chars = size = sizeof(s2) - 1;
+    bytes = ascii_count(s2, size, &chars);
+    cr_expect(bytes == size, "ascii_count(s2) return incorrect bytes: expect %d, got %d", size, bytes);
+    cr_expect(chars == size, "ascii_count(s2) return incorrect chars: expect %d, got %d", size, chars);
+
+    chars = sizeof(s3);
+    size = sizeof(s3) - 1;
+    bytes = ascii_count(s3, size, &chars);
+    cr_expect(bytes == size, "ascii_count(s3) return incorrect bytes: expect %d, got %d", size, bytes);
+    cr_expect(chars == size, "ascii_count(s3) return incorrect chars: expect %d, got %d", size, chars);
+
+    chars = size = sizeof(b1) - 1;
+    bytes = ascii_count(b1, size, &chars);
+    cr_expect(bytes == -1, "ascii_count(b1) return incorrect bytes: expect %d, got %d", -1, bytes);
+    cr_expect(chars == size, "ascii_count(b1) return incorrect chars: expect %d, got %d", size, chars);
+
+    chars = size = sizeof(b2) - 1;
+    bytes = ascii_count(b2, size, &chars);
+    cr_expect(bytes == -1, "ascii_count(b2) return incorrect bytes: expect %d, got %d", -1, bytes);
+    cr_expect(chars == size, "ascii_count(b2) return incorrect chars: expect %d, got %d", size, chars);
+
+    chars = size = sizeof(b3) - 1;
+    bytes = ascii_count(b3, size, &chars);
+    cr_expect(bytes == -1, "ascii_count(b3) return incorrect bytes: expect %d, got %d", -1, bytes);
+    cr_expect(chars == size, "ascii_count(b3) return incorrect chars: expect %d, got %d", size, chars);
+} // ascii_count
