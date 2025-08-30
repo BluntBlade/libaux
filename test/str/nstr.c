2e9c4d07c9315f3a9d3327c75712387de741567f
Liang Tao <amethyst.black@gmail.com>
2025-08-30 16:16:22 +0800

    ADD: 添加测试用例。

--- test/str/nstr.c
+++ test/str/nstr.c
@@ -5,12 +5,14 @@
 #include "str/nstr.c"
 #endif
 
-Test(Function, nstr_blank_string)
+Test(Function, nstr_blank)
 {
     nstr_p blk = nstr_blank();
 
-    cr_expect(blk != NULL, "nstr_blank() return pointer: expect non-NULL, got NULL");
-    cr_expect(blank_dummy.slcs == 1, "nstr_blank_string() don't add references: expect %d, got %d", 1, blank_dummy.slcs);
+    cr_expect(blk = &blank, "nstr_blank() return pointer: expect %p, got %p", &blank, blk);
+    cr_expect(blank_dummy.slcs == 1, "nstr_blank() don't add references: expect %d, got %d", 1, blank_dummy.slcs);
+    cr_expect(blank_dummy.need_free == 0);
+    cr_expect(blank.need_free == 0);
 
     nstr_delete(blk);
-} // nstr_blank_string
+} // nstr_blank
